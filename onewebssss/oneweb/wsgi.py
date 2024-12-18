284b16e12ac01b9a7e7857bfc2c1a02bbdb61469
Emeusa <98482021+Emeusa@users.noreply.github.com>
2024-12-18 10:55:40 +0100

    Update wsgi.py

--- onewebssss/oneweb/wsgi.py
+++ onewebssss/oneweb/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os, sys
 
+sys.path.append('/Aoneeducationalreps/onewebssss/oneweb')
+sys.path.append('/onewebssss/oneweb/')
+sys.path.append('/oneweb/')
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onewebssss.oneweb.settings')
 
 application = get_wsgi_application()
